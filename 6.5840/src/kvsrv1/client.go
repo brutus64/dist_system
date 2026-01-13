374777cbeb3467aeaf7dd7fbe796363b239c20fb
brutus64 <109479798+brutus64@users.noreply.github.com>
2026-01-13 17:54:19 -0500

    part 2 lock complete

--- 6.5840/src/kvsrv1/client.go
+++ 6.5840/src/kvsrv1/client.go
@@ -1,13 +1,10 @@
 package kvsrv
 
 import (
-	"fmt"
-
 	"6.5840/kvsrv1/rpc"
-	"6.5840/kvtest1"
-	"6.5840/tester1"
+	kvtest "6.5840/kvtest1"
+	tester "6.5840/tester1"
 )
-
 
 type Clerk struct {
 	clnt   *tester.Clnt
@@ -32,7 +29,7 @@ func MakeClerk(clnt *tester.Clnt, server string) kvtest.IKVClerk {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 	// You will have to modify this function.
-	args := rpc.GetArgs{Key:key}
+	args := rpc.GetArgs{Key: key}
 	reply := rpc.GetReply{}
 	ok := ck.clnt.Call(ck.server, "KVServer.Get", &args, &reply)
 	for !ok {
--- 6.5840/src/kvsrv1/lock/lock.go
+++ 6.5840/src/kvsrv1/lock/lock.go
@@ -1,7 +1,8 @@
 package lock
 
 import (
-	"6.5840/kvtest1"
+	"6.5840/kvsrv1/rpc"
+	kvtest "6.5840/kvtest1"
 )
 
 type Lock struct {
@@ -11,6 +12,8 @@ type Lock struct {
 	// MakeLock().
 	ck kvtest.IKVClerk
 	// You may add code here
+	lockKey  string
+	clientID string
 }
 
 // The tester calls MakeLock() and passes in a k/v clerk; your code can
@@ -19,15 +22,52 @@ type Lock struct {
 // Use l as the key to store the "lock state" (you would have to decide
 // precisely what the lock state is).
 func MakeLock(ck kvtest.IKVClerk, l string) *Lock {
-	lk := &Lock{ck: ck}
+	lk := &Lock{ck: ck, lockKey: l, clientID: kvtest.RandValue(8)}
 	// You may add code here
 	return lk
 }
 
 func (lk *Lock) Acquire() {
-	// Your code here
+	/*
+		idea:
+		get the key first
+		check if that key is empty
+		if it is, put into that lock, the clientID
+		else key is clientID itself
+		we already acquired it just ignore dont do anymore operations
+		else key is a clientID not itself
+		we cannot do anything to acquire so just do nothing
+	*/
+	for {
+		val, ver, err := lk.ck.Get(lk.lockKey)
+		if err == rpc.ErrNoKey {
+			err = lk.ck.Put(lk.lockKey, lk.clientID, 0)
+			if err == rpc.OK {
+				return
+			}
+			continue //put failed, have it get the value/ver of lock again
+		}
+		switch val {
+		case "":
+			err = lk.ck.Put(lk.lockKey, lk.clientID, ver)
+			if err == rpc.OK {
+				return
+			}
+		case lk.clientID: //already have lock
+			return
+		default:
+			continue //keep getting until its not held by another client
+		}
+	}
 }
 
 func (lk *Lock) Release() {
-	// Your code here
+	val, ver, err := lk.ck.Get(lk.lockKey)
+	if val != lk.clientID || err == rpc.ErrNoKey { //not held by us
+		return
+	}
+	err = lk.ck.Put(lk.lockKey, "", ver)
+	for err != rpc.OK {
+		err = lk.ck.Put(lk.lockKey, "", ver)
+	}
 }
